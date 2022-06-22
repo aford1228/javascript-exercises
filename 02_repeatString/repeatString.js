cf4eb7773dd6c20a74d6b01b38a1e6095870d815
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-22 17:26:42 -0400

    Add comments for clarity

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,13 @@
 const repeatString = (str, num) => {
+  // initialize new string as an empty string
   let repeated = '';
+  // exit function if the string repeats 0 or fewer times
   if (num < 0) {return 'ERROR';}
+  // continue the loop as long as the counter is not 0
   while (num > 0) {
+    // concatenate the string
     repeated = repeated.concat(str);
+    // decrement the counter
     num--;
   };
   return repeated;
