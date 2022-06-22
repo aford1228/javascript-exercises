845ed2cdcf5e33153c2d62b3c6ac186856f20396
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-22 18:00:25 -0400

    Spacing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,6 @@
 const ftoc = (f) => Math.round(((f - 32) * (5 / 9)) * 10) / 10;
 
-const ctof = (c) => Math.round(((c * (5/9)) + 32) * 10) / 10;
+const ctof = (c) => Math.round(((c * (5 / 9)) + 32) * 10) / 10;
 
 console.log(ctof(73.2));
 
