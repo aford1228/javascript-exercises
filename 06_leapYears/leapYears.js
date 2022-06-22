95a1381587bcddec832b9cc03c3b6cbe60c2c50d
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-22 17:27:16 -0400

    Comment clarity

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,7 +2,7 @@ const leapYears = (year) => {
   // if year is divisible by for
   return (year % 4 == 0)
   // and if year is NOT divisible by 100
-  // or if year is divisible by 400
+  // or if year IS divisible by 400
   && (year % 100 != 0 || year % 400 == 0) 
   ? true 
   : false;
