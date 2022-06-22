b79d94776e844d193dc0899f6d0c8738e1657c32
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-22 17:24:18 -0400

    Add comments for clarity

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,3 +1,7 @@
+// initialize function expression accepting a string as argument
+// split the string into an array of each individual character
+// reverse the order of the array
+// join the array into a string of each character
 const reverseString = (str) => str.split('').reverse().join('');
 
 // Do not edit below this line
