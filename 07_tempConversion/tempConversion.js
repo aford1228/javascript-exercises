29d7a3d12818dcdb93d912989fd729fde3199711
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-22 17:44:57 -0400

    Set up basic formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,8 @@
-const ftoc = function() {
+const ftoc = (f) => Math.round(((f - 32) * (5 / 9)) * 10) / 10;
 
-};
+const ctof = (c) => Math.round(((c * (5/9)) + 32) * 10) / 10;
 
-const ctof = function() {
-
-};
+console.log(ctof(73.2));
 
 // Do not edit below this line
 module.exports = {
