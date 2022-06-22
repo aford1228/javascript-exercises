d9bcf18e4ab3bf610f76a799aaeeaa9370ed9c7b
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-22 14:22:00 -0400

    Complete challenges 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = (str, num) => {
+  let repeated = '';
+  if (num < 0) {return 'ERROR';}
+  while (num > 0) {
+    repeated = repeated.concat(str);
+    num--;
+  };
+  return repeated;
 };
 
 // Do not edit below this line
