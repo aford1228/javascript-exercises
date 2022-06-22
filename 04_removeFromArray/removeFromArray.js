49f1607c7fb483145f5780a544637c3b612853d5
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-22 15:42:53 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = (arr, ...n) => arr.filter(i => n.indexOf(i) === -1);
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
