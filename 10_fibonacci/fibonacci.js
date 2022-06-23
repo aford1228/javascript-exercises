db7d35f770d4b9cd20fa9f6cf8dd5d825e76d50b
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-23 10:09:30 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
+const power = (n, exp) => {
+  let pow = 1;
+  while (exp != 0) {
+    pow *= n;
+    exp--;
+  }
+  return pow;
+}
+
+const phi = (1 + Math.sqrt(5)) / 2;
+const phi2 = -1 / phi;
+
 const fibonacci = (n) => {
-  
-};
+  if (n > 0) {
+    return Math.floor((power(phi, n) - power(phi2, n)) / Math.sqrt(5));
+  }
+  else {return `OOPS`;};
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
