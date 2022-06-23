e37ef7b2615f6f3d98382e18b91f2f6278f9a8f3
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-23 09:41:02 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = (str) => {
+  let forward = str.toLowerCase().replace(/[^a-zA-Z]/gi, '');
+  let reverse = str.toLowerCase().replace(/[^a-zA-Z]/gi, '').split('').reverse().join('');
+  console.log(forward, reverse);
+  if (forward === reverse) {
+    return true;
+  }
+  else {return false};
 };
 
 // Do not edit below this line
