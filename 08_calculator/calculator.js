f70be3310a5be99ecc5155fbe8432a9dc19e9479
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-23 09:24:14 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = ([...n]) => {
+	let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum = sum + n[i];
+  };
+  return sum;
 };
 
-const subtract = function() {
-	
+const multiply = ([...n]) => {
+  let product = 1;
+  for (let i = 0; i < n.length; i++) {
+    product = product * n[i];
+  };
+  return product;
 };
 
-const sum = function() {
-	
-};
+const power = (n, exp) => {
+  let pow = 1;
+  while (exp != 0) {
+    pow *= n;
+    exp--;
+  }
+  return pow;
+}
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (n) => {
+	let fac = 1;
+  while (n > 0) {
+    fac *= n;
+    n--;
+  };
+  return fac;
 };
 
 // Do not edit below this line
