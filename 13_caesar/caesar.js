3348bd53dd21bb0f7ba144bac673699b4d8d1240
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-24 11:30:34 -0400

    Write split and join expressions

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,11 @@
-const caesar = function() {
+const caesar = (str, shift) => {
+  // split string into an array of unicode values
+  let cipher = str.split('').map(char => char.charCodeAt());
 
-};
+  return cipher.map(char => String.fromCharCode(char)).join('');
+}
+
+console.log(caesar('Aaa', -1));
 
 // Do not edit below this line
 module.exports = caesar;
